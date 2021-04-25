5cf1d5c9139785edae6e7b7e652a88587f8376dc
John <juaorok@hotmail.com>
2021-04-25 18:36:29 -0300

    Create remove_acentos.php

---
+++ remove_acentos.php
@@ -0,0 +1,5 @@
+<?php
+
+function stripAccents($stripAccents){
+  return strtr($stripAccents, 'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ', 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
+}
